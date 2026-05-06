97df3c5fb69597201b9fc3702b5e062e0aec038f
Johan Vallejos <johan.vallejos@tecsup.edu.pe>
2026-05-06 15:38:04 -0500

    Corrigiendo respuesta del servidor para Render

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 const http = require('http')
 
-function requestController() {
+// Agregamos (req, res) para poder interactuar con el navegador
+function requestController(req, res) {
     console.log('Bienvenidos al curso')
+    
+    // Esto es lo que verá el profesor cuando abra tu link de Render
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Servidor funcionando correctamente en la nube!')
 }
 
 const server = http.createServer(requestController)
 
-const PORT = process.env.PORT
+// Usamos el puerto de la variable de entorno, o el 4000 por defecto
+const PORT = process.env.PORT || 4000
 
 server.listen(PORT, function() {
     console.log("Aplicacion corriendo en: " + PORT)
